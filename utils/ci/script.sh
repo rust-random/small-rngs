ec78857a956cbb131adc7a028a88959cadc58562
Diggory Hardy <git@dhardy.name>
2018-10-01 12:57:13 +0100

    Fix CI tests (Cargo workaround)
    See Cargo issue 5364

--- utils/ci/script.sh
+++ utils/ci/script.sh
@@ -9,7 +9,8 @@ main() {
     fi
 
     cross test --target $TARGET --all
-    cross test --target $TARGET --package rand_pcg --features serde1
+    cross test --target $TARGET --manifest-path rand_pcg/Cargo.toml --package rand_pcg --features serde1
+    cross test --target $TARGET --manifest-path rand_xorshift/Cargo.toml --package rand_xorshift --features serde1
 }
 
 # we don't run the "test phase" when doing deploys
